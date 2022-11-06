783d9b687fe44164d1d510cf7ebe2f83d144b081
Anushka Singh Raghuvanshi <114311721+Theanushkaraghuvanshi@users.noreply.github.com>
2022-11-06 20:40:01 +0530

    work2

---
+++ immutable.py
@@ -0,0 +1,12 @@
+
+
+mystring="this is mystring"
+anotherstring="THIS IS ANOTHER STRING"
+
+print(mystring.capitalize())
+print(mystring.upper())
+print(mystring.lower())
+print(mystring.title())
+print(mystring.swapcase())
+print(anotherstring.swapcase())
+
