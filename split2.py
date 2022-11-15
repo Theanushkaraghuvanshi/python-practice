50a241050587da521c66faf28393cb16fd8bc765
Anushka Singh Raghuvanshi <114311721+Theanushkaraghuvanshi@users.noreply.github.com>
2022-11-15 20:32:49 +0530

    work8

---
+++ split2.py
@@ -0,0 +1,8 @@
+data1=input("data1:")
+list1=data1.split(",")
+data2=input("data2:")
+list2=data2.split(",")
+if list[0]==list2[0] or list1[-1]==list2[-1]:
+    print("True")
+else:
+    print("False")
